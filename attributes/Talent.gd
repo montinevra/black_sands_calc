b7376e2d529fe85d28d19f10c9fdd19354f88cec
Montinevra <montinevra@gmail.com>
2020-08-15 11:25:00 -0700

    remove unused code

--- attributes/Talent.gd
+++ attributes/Talent.gd
@@ -6,8 +6,7 @@ var m_natural = 5
 
 
 func _ready():
-	Events.connect("race_changed", self, "_on_natural_changed")
-	pass
+	Events.connect("race_changed", self, "_on_race_changed")
 
 
 func _on_Rank_value_changed(value):
@@ -16,26 +15,29 @@ func _on_Rank_value_changed(value):
 	if baseline < 0:
 		m_points.value = baseline * 5
 	else:
-		m_points.value = _get_rank_cost(m_natural, value)
+		m_points.value = _get_rank_cost(value)
 
 
 func _on_Points_value_changed(value):
 	._on_Points_value_changed(value)
 
 
-func _on_natural_changed(t_naturals):
-	var new_natural = t_naturals[m_name]
+func _on_race_changed(t_new_naturals):
+	var new_natural = t_new_naturals[m_name]
+	
+	_update_natural(new_natural)
 
-	m_natural = new_natural
-	m_rank.min_value = new_natural - 3
-	m_rank.max_value = new_natural + 5
-	m_points.max_value = _get_rank_cost(new_natural, new_natural + 5)
+
+func _update_natural(t_new_natural):
+	m_natural = t_new_natural
+	m_rank.min_value = t_new_natural - 3
+	m_rank.max_value = t_new_natural + 5
 	_on_Rank_value_changed(m_rank.value)
 
 
-func _get_rank_cost(t_natural, t_rank) -> int:
+func _get_rank_cost(t_rank) -> int:
 	var cost = 0
 	
-	for x in range(t_natural + 1, t_rank + 1):
+	for x in range(m_natural + 1, t_rank + 1):
 		cost += (x) * (x - m_natural)
 	return cost
